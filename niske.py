f4b53646210c7160ba7ab6094f849e4d20968157
Branka-Knezevic <branka.370833@gmail.com>
2017-06-30 16:29:25 +0200

    Add files via upload

---
+++ niske.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+r="Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+r=r.lower()
+lista_reči=r.split(" ")
+print (lista_reči)
+
+
+niska = "Dokle ću više biti niska!"
+karakteri = list(niska)
+print(karakteri)
+
+
+

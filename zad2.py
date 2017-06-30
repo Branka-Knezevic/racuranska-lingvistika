6945102219edefbd224a4db23be8a98079e99932
Branka-Knezevic <branka.370833@gmail.com>
2017-06-30 17:02:57 +0200

    Add files via upload

---
+++ zad2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+reč1 = "pevač"
+reč1_osnova=reč1[0:3]
+reč1_osnova=reč1[-5:-2]
+print(reč1_osnova)
+reč1_sufiks=reč1[3:]
+print(reč1_sufiks)
+
+reč2="beogradski"
+reč2_osnova=reč2[0:7]
+print(reč2_osnova)
+reč2_sufiks=reč2[7:11]
+
+reč3="spavanje"
+reč3_osnova=reč3[0:4]
+reč3_tematski_vokal=reč3[4]
+reč3_sufiks=reč3[5:8]
+

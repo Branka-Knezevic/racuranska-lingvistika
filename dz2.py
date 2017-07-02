09369095a7333d852e0779087dfaf626b2984a13
Branka-Knezevic <branka.370833@gmail.com>
2017-07-02 02:49:58 +0200

    Add files via upload

---
+++ dz2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+rečnik={"smejati se":{"sinonimi":["kikotati se","smeškati se"],"antonimi":["plakati","mrštiti se"]},"dobar":{"sinonimi":["nepokvaren","milosrdan"],"antonimi":["loš","zao"]},"veliki":{"sinonimi":["ogroman","glomazan"],"antonimi":["mali","sićušan"]}}
+
+print(rečnik["smejati se"]["sinonimi"])
+print(rečnik["dobar"]["antonimi"])
+print(rečnik["veliki"]["antonimi"][1])
+print(rečnik["dobar"]["sinonimi"][0])
+
